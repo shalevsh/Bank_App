3d1479320cb36ef0339880e235f3949760e1a60e
shalev <shalevsharf@gmail.com>
2022-11-22 07:57:13 +0200

    consts error handling

--- backend/server_utils/routers/error_handler.py
+++ backend/server_utils/routers/error_handler.py
@@ -1,19 +1,28 @@
 from fastapi import HTTPException, status
 import math
 
+BAD_REQUEST_MSG = "General error pls refresh the page"
+TRANSACTION_AMOUNT = "Invalid transaction amount"
+TYPE_TRANSACTION_AMOUNT = "Invalid transaction type"
+TRANSACTION_OPERTATION = "Invalid transaction operation"
+CATEGORY_NAME = "Invalid category name"
+VENDOR_NAME = "Invalid category vendor"
+
+
+
 
 def post_request(client_data: dict):
     if client_data == None:
-        raise ValueError("General error pls refresh the page")
+        raise ValueError(BAD_REQUEST_MSG)
     if client_data.amount <= 0:
-        raise ValueError("Invalid transaction amount")
+        raise ValueError(TRANSACTION_AMOUNT)
     if math.isnan(client_data.amount):
-        raise TypeError("Invalid transaction type")
+        raise TypeError(TYPE_TRANSACTION_AMOUNT)
     if (not isinstance(client_data.is_deposit, (bool))):
-        raise TypeError("Invalid transaction type")
+        raise TypeError(TRANSACTION_OPERTATION)
     if (not isinstance(client_data.category, (str))):
-        raise TypeError("Invalid category name")
+        raise TypeError(CATEGORY_NAME)
     if (not isinstance(client_data.vendor, (str))):
-        raise TypeError("Invalid category vendor")
+        raise TypeError(VENDOR_NAME)
 
 
